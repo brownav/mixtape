a6dd4ead50b31596fa0525b2fe3693725562ce27
Alexandria Brown <axbn2190@gmail.com>
2018-05-06 22:18:42 -0700

    added songs controller

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,8 @@
+class SongsController < ApplicationController
+
+  def index
+    songs = Song.all
+    render :json => songs.as_json(only: [:id, :title, :artist, :year]), status: :ok
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'songs/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get '/songs', to: 'songs#index', as: 'songs'
+  get '/songs', to: 'songs#index', as: 'songs'
 end
